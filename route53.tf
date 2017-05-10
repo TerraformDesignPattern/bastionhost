ab0befb3ff3fcb8a90c292debee9dbc6b3acfb88
Jon Brouse <jbrouse19@gmail.com>
2017-05-09 21:26:56 -0400

    Fixed zone_id in route53.tf

--- route53.tf
+++ route53.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "route53_record" {
-  zone_id = "${data.terraform_remote_state.account.primary_zone_id}"
+  zone_id = "${data.terraform_remote_state.account.route53_zone_id}"
   name    = "${var.hostname}.${data.terraform_remote_state.account.domain_name}"
   type    = "A"
   ttl     = "300"
